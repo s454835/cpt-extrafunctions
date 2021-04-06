3fb3286e2c2030c648cb035bb3abacc65395d3d3
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-04-06 10:04:24 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Created by: Tim")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.House)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.StickFigure)
+})
