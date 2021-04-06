b6abe38b31e0a553c08cf217aabf0a950fda1b58
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-04-06 14:00:53 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Created by: Tim")
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(input.compassHeading())
     music.playMelody("E B C5 A B G A F ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.temperature())
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(0, 6))
 })
